357ca14b3ee23de74b255eada581563e6793964b
roshan <102507924+roshanpanda666@users.noreply.github.com>
2023-01-01 17:42:12 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+document.querySelector("button").addEventListener("click", fun1);
+
+function fun1() {
+  document.querySelector(".txt").innerHTML = "welcome to the new matrix version 2023<br>hope you will do some magical stuff this year <3";
+  document.querySelector("h1").style="display: block"
+  document.querySelector(".me").style="display:block"
+}
